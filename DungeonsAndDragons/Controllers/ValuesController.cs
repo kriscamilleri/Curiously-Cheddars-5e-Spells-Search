37857ab374ebd6ad7c3d76dd3f361887d01de764
Kris Camilleri <kris.camilleri@gmail.com>
2018-08-17 14:25:47 +0200

    cleaned up loftover files

--- DungeonsAndDragons/Controllers/ValuesController.cs
+++ DungeonsAndDragons/Controllers/ValuesController.cs
@@ -11,7 +11,6 @@ namespace DungeonsAndDragons.Controllers
     [Route("api/[controller]/[action]")]
     public class ValuesController : Controller
     {
-
         private string _connectionString = "mongodb://localhost:27017/local";
 
         [HttpGet]
@@ -34,43 +33,11 @@ namespace DungeonsAndDragons.Controllers
             return spell;
         }
 
-        [HttpGet]
         private IMongoDatabase GetDatabase()
         {
             var _databaseName = MongoUrl.Create(_connectionString).DatabaseName;
             return new MongoClient(_connectionString).GetDatabase(_databaseName);
         }
 
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
