6e056ab7f564e492d15710fccb718fc2b73b0081
kriscamilleri <kris.camilleri@gmail.com>
2019-08-24 21:28:43 +0200

    flattening folder structure for netlify deployment

---
+++ src/App.vue
@@ -0,0 +1,364 @@
+<template>
+  <div id="app">
+    <meta name="viewport" content="width=device-width, user-scalable=false;" />
+
+    <spell-nav-bar
+      @sideBarOn="captureSideBarStatus"
+      @searchText="captureSearchText"
+      :sideBarOn="sideBarOn"
+    ></spell-nav-bar>
+    <div id="wrapper" :class="{ toggled: sideBarOn }">
+      <div id="sidebar-wrapper" class="bg-light">
+        <spell-filters
+          :classFilters="classFilters"
+          @classFilters="captureClassFilters"
+          :levelFilters="levelFilters"
+          @levelFilters="captureLevelFilters"
+          :sourceFilters="sourceFilters"
+          @sourceFilters="captureSourceFilters"
+          :schoolFilters="schoolFilters"
+          @schoolFilters="captureSchoolFilters"
+        ></spell-filters>
+      </div>
+      <!-- <add-spell></add-spell> -->
+      <div id="page-content-wrapper">
+        <b-container fluid class="bv-example-row m-1">
+          <b-row :class="{ 'd-none': dataLoading }" align-h="center">
+            <spell-card v-for="r in filteredSpells" :spell="r" :key="r.index" class="m-2"></spell-card>
+          </b-row>
+          <div v-if="dataLoading" class="text-center p-5">
+            <h2>Loading data...</h2>
+          </div>
+        </b-container>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SpellNavBar from "./SpellNavBar.vue";
+import SpellCard from "./SpellCard.vue";
+import AddSpell from "./AddSpell.vue";
+import SpellFilters from "./SpellFilters.vue";
+
+export default {
+  components: {
+    SpellCard,
+    // AddSpell,
+    SpellNavBar,
+    SpellFilters
+  },
+  data() {
+    return {
+      title: "DnD Search Master",
+      dataLoading: true,
+      sideBarOn: false,
+      searchText: "",
+      spells: [],
+      classFilters: [
+        "bard",
+        "cleric",
+        "druid",
+        "paladin",
+        "ranger",
+        "sorcerer",
+        "warlock",
+        "wizard"
+      ],
+      sourceFilters: ["EE PC", "PHB", "SCAG", "UA TOBM"],
+      levelFilters: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      schoolFilters: [
+        "Abjuration",
+        "Conjuration",
+        "Divination",
+        "Enchantment",
+        "Evocation",
+        "Illusion",
+        "Necromancy",
+        "Transmutation"
+      ],
+      classes: [
+        "bard",
+        "cleric",
+        "druid",
+        "paladin",
+        "ranger",
+        "sorcerer",
+        "warlock",
+        "wizard"
+      ],
+      levels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      sources: ["EE PC", "PHB", "SCAG", "UA TOBM"],
+      schools: [
+        "Abjuration",
+        "Conjuration",
+        "Divination",
+        "Enchantment",
+        "Evocation",
+        "Illusion",
+        "Necromancy",
+        "Transmutation"
+      ]
+    };
+  },
+  computed: {
+    filteredSpells: function() {
+      var searchText = this.searchText;
+      let spells = this.spells;
+
+      spells = this.filterSearch(spells, searchText);
+      spells = this.filterClasses(spells);
+      spells = this.filterLevels(spells);
+      spells = this.filterSources(spells);
+      spells = this.filterSchools(spells);
+
+      return spells;
+    }
+  },
+  methods: {
+    captureSideBarStatus(sideBarOn) {
+      this.sideBarOn = sideBarOn;
+    },
+    captureSearchText(searchText) {
+      this.searchText = searchText;
+    },
+    captureClassFilters(value) {
+      this.classFilters = value;
+    },
+    captureLevelFilters(value) {
+      this.levelFilters = value;
+    },
+    captureSourceFilters(value) {
+      this.sourceFilters = value;
+    },
+    captureSchoolFilters(value) {
+      this.schoolFilters = value;
+    },
+    filterSearch(spells, searchText) {
+      if (searchText.length > 0) {
+        spells = spells.filter(function(spell) {
+          let lowerText = searchText.toLowerCase();
+          return (
+            spell.name
+              .toLowerCase()
+              .trim()
+              .indexOf(lowerText) > -1
+          );
+        });
+      }
+      return spells;
+    },
+    filterClasses(spells) {
+      let classFilters = this.classFilters.map(function(value) {
+        return value[0].toUpperCase() + value.slice(1);
+      });
+
+      if (this.classFilters.length != this.classes.length) {
+        spells = spells.filter(function(spell) {
+          let x = spell.class.split(",");
+          let intersection = x.filter(n => classFilters.includes(n));
+          if (intersection.length > 0) {
+            return spell;
+          }
+        });
+      }
+      return spells;
+    },
+    filterLevels(spells) {
+      let levelFilters = this.levelFilters;
+      if (levelFilters.length != this.levels.length) {
+        spells = spells.filter(function(spell) {
+          let x = spell.level;
+          let intersection = levelFilters.includes(x);
+          if (intersection) {
+            return spell;
+          }
+        });
+      }
+      return spells;
+    },
+    filterSources(spells) {
+      let sourceFilters = this.sourceFilters;
+      if (sourceFilters.length != this.sources.length) {
+        spells = spells.filter(function(spell) {
+          let x = spell.source;
+          let intersection = sourceFilters.includes(x);
+          if (intersection) {
+            return spell;
+          }
+        });
+      }
+      return spells;
+    },
+    filterSchools(spells) {
+      let schoolFilters = this.schoolFilters;
+      if (schoolFilters.length != this.schools.length) {
+        spells = spells.filter(function(spell) {
+          let x = spell.school;
+          let intersection = schoolFilters.includes(x);
+          if (intersection) {
+            return spell;
+          }
+        });
+      }
+      return spells;
+    }
+  },
+  mounted() {
+    let self = this;
+    fetch("https://vigilant-montalcini-5f08ae.netlify.com/literuje.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        let parsedData = [];
+        for (let i = 0; i < data.length; i++) {
+          data[i].index = i;
+          parsedData.push(data[i]);
+        }
+        // console.log(data);
+        this.spells = parsedData;
+        this.dataLoading = false;
+      });
+  }
+};
+</script>
+
+<style>
+/*.card-columns {
+    display: inline-block;
+    column-break-inside: avoid;
+  }*/
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #007bff;
+}
+
+#app {
+  margin-right: 15px;
+  margin-right: 15px;
+}
+
+/* SIDEBAR */
+body {
+  overflow-x: hidden;
+}
+
+#wrapper {
+  padding-right: 0;
+  -webkit-transition: all 0.5s ease;
+  -moz-transition: all 0.5s ease;
+  -o-transition: all 0.5s ease;
+  transition: all 0.5s ease;
+}
+
+#wrapper.toggled {
+  padding-right: 380px;
+}
+
+#sidebar-wrapper {
+  z-index: 1029;
+  position: fixed;
+  right: 380px;
+  width: 0;
+  height: 100%;
+  margin-right: -380px;
+  overflow-y: auto;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  margin-top: -10px;
+}
+
+#wrapper.toggled #sidebar-wrapper {
+  width: 380px;
+}
+
+#page-content-wrapper {
+  width: 100%;
+  position: absolute;
+  padding: 0px;
+}
+
+#wrapper.toggled #page-content-wrapper {
+  position: absolute;
+  margin-left: -380px;
+}
+
+/* Sidebar Styles */
+
+.sidebar-nav {
+  position: absolute;
+  top: 0;
+  width: 380px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.sidebar-nav > .sidebar-brand {
+  height: 65px;
+  font-size: 18px;
+  line-height: 60px;
+}
+
+.sidebar-nav > .sidebar-brand a {
+  color: #999999;
+}
+
+.sidebar-nav > .sidebar-brand a:hover {
+  color: #fff;
+  background: none;
+}
+
+@media (min-width: 768px) {
+  #wrapper {
+    padding-right: 0;
+  }
+
+  #sidebar-wrapper {
+    width: 0;
+  }
+  #wrapper.toggled #sidebar-wrapper {
+    width: 380px;
+  }
+  #page-content-wrapper {
+    position: relative;
+  }
+  #wrapper.toggled #page-content-wrapper {
+    position: relative;
+    margin-left: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  .h1,
+  .h2,
+  .h3,
+  .h4,
+  .h5,
+  .h6 {
+    font-weight: 400;
+  }
+}
+</style>
---
+++ src/SpellFilters.vue
@@ -0,0 +1,242 @@
+<template>
+    <b-row class="m-2">
+        <b-col sm=12>
+            <h3 class="my-2 mt-4">Filters</h3>
+        </b-col>
+        <b-col sm="12">
+          <b-row>
+            <span class="col-6 align-text-bottom">
+                <strong class="filter-header">Class</strong>
+                <hr>
+            </span>
+            <span class="col-6 align-middle pt-2">
+                <label :class="classAllSelected ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-outline-primary'">
+                      <b-form-checkbox v-model="classAllSelected"
+                          aria-describedby="selectedClassFilters"
+                          aria-controls="selectedClassFilters"
+                          @change="toggleAllClasses"
+                          class="">
+                      Select All
+                    </b-form-checkbox>
+                </label>
+              </span>
+            </b-row>
+            <b-form-group >
+                <b-form-checkbox-group v-model="selectedClassFilters" class="faux-column row"   :options="getClassOptions">
+                </b-form-checkbox-group>
+            </b-form-group>
+        </b-col>
+        <b-col sm="12">
+          <b-row>
+            <span class="col-6 align-text-bottom">
+              <strong class="filter-header">Level</strong>
+              <hr>
+            </span>
+            <span class="col-6 align-middle pt-2">
+                <label :class="levelAllSelected ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-outline-primary'">
+                      <b-form-checkbox v-model="levelAllSelected"
+                          aria-describedby="selectedLevelFilters"
+                          aria-controls="selectedLevelFilters"
+                          @change="toggleAllLevels"
+                          class="">
+                      Select All
+                    </b-form-checkbox>
+                </label>
+              </span>
+            </b-row>
+            <b-form-group>
+                <b-form-checkbox-group v-model="selectedLevelFilters" class="faux-column row"  :options="getLevelOptions">
+                </b-form-checkbox-group>
+            </b-form-group>
+        </b-col>
+        <b-col sm="12">
+          <b-row>
+            <span class="col-6 align-text-bottom">
+              <strong class="filter-header">School</strong>
+              <hr>
+            </span>
+            <span class="col-6 align-middle pt-2">
+                <label :class="schoolAllSelected ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-outline-primary'">
+                      <b-form-checkbox v-model="schoolAllSelected"
+                          aria-describedby="selectedSchoolFilters"
+                          aria-controls="selectedSchoolFilters"
+                          @change="toggleAllSchools"
+                          class="">
+                      Select All
+                    </b-form-checkbox>
+                </label>
+              </span>
+            </b-row>
+            <b-form-group >
+                <b-form-checkbox-group v-model="selectedSchoolFilters" class="faux-column row"  :options="getSchoolOptions">
+                </b-form-checkbox-group>
+            </b-form-group>
+        </b-col>
+        <b-col sm="12">
+          <b-row>
+            <span class="col-6 align-text-bottom">
+              <strong class="filter-header">Source</strong>
+              <hr>
+            </span>
+            <span class="col-6 align-middle pt-2">
+                <label :class="sourceAllSelected ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-outline-primary'">
+                      <b-form-checkbox v-model="sourceAllSelected"
+                          aria-describedby="selectedSourceFilters"
+                          aria-controls="selectedSourceFilters"
+                          @change="toggleAllSources"
+                          class="">
+                      Select All
+                    </b-form-checkbox>
+                </label>
+              </span>
+            </b-row>
+            <b-form-group>
+                <b-form-checkbox-group v-model="selectedSourceFilters" class="faux-column row"   :options="getSourceOptions">
+                </b-form-checkbox-group>
+            </b-form-group>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+  name: "SpellFilters",
+  props: {
+    classFilters: Array,
+    sourceFilters: Array,
+    levelFilters: Array,
+    schoolFilters: Array
+  },
+  data: function() {
+    return {
+      localClassFilters: this.classFilters,
+      selectedClassFilters: this.classFilters,
+      localLevelFilters: this.levelFilters,
+      selectedLevelFilters: this.levelFilters,
+      localSourceFilters: this.sourceFilters,
+      selectedSourceFilters: this.sourceFilters,
+      localSchoolFilters: this.schoolFilters,
+      selectedSchoolFilters: this.schoolFilters,
+      classAllSelected: true,
+      levelAllSelected: true,
+      schoolAllSelected: true,
+      sourceAllSelected: true
+    };
+  },
+  computed: {
+    getClassOptions: function() {
+      let arr = [];
+      for (let i = 0; i < this.localClassFilters.length; i++) {
+        let row = {
+          text:
+            this.localClassFilters[i][0].toUpperCase() +
+            this.localClassFilters[i].slice(1),
+          value: this.localClassFilters[i]
+        };
+        arr.push(row);
+      }
+      return arr;
+    },
+    getLevelOptions: function() {
+      let arr = [];
+      for (let i = 0; i < this.localLevelFilters.length; i++) {
+        let row = {
+          text: this.localLevelFilters[i],
+          value: this.localLevelFilters[i]
+        };
+        arr.push(row);
+      }
+      return arr;
+    },
+    getSourceOptions: function() {
+      let arr = [];
+      for (let i = 0; i < this.localSourceFilters.length; i++) {
+        let row = {
+          text: this.localSourceFilters[i],
+          value: this.localSourceFilters[i]
+        };
+        arr.push(row);
+      }
+      return arr;
+    },
+    getSchoolOptions: function() {
+      let arr = [];
+      for (let i = 0; i < this.localSchoolFilters.length; i++) {
+        let row = {
+          text: this.localSchoolFilters[i],
+          value: this.localSchoolFilters[i]
+        };
+        arr.push(row);
+      }
+      return arr;
+    }
+  },
+  methods: {
+    toggleAllClasses(checked) {
+      this.selectedClassFilters = this.classAllSelected
+        ? []
+        : this.localClassFilters;
+    },
+    toggleAllLevels(checked) {
+      this.selectedLevelFilters = this.levelAllSelected
+        ? []
+        : this.localLevelFilters;
+    },
+    toggleAllSchools(checked) {
+      this.selectedSchoolFilters = this.schoolAllSelected
+        ? []
+        : this.localSchoolFilters;
+    },
+    toggleAllSources(checked) {
+      this.selectedSourceFilters = this.sourceAllSelected
+        ? []
+        : this.localSourceFilters;
+    }
+  },
+  watch: {
+    selectedClassFilters(newVal, oldVal) {
+      this.classAllSelected =
+        this.selectedClassFilters.length == this.localClassFilters.length;
+
+      this.$emit("classFilters", newVal);
+    },
+    selectedLevelFilters(val) {
+      this.levelAllSelected =
+        this.selectedLevelFilters.length == this.localLevelFilters.length;
+
+      this.$emit("levelFilters", val);
+    },
+    selectedSchoolFilters(val) {
+      this.schoolAllSelected =
+        this.selectedSchoolFilters.length == this.localSchoolFilters.length;
+
+      this.$emit("schoolFilters", val);
+    },
+    selectedSourceFilters(val) {
+      this.sourceAllSelected =
+        this.selectedSourceFilters.length == this.localSourceFilters.length;
+
+      this.$emit("sourceFilters", val);
+    }
+  }
+};
+</script>
+<style>
+.faux-column div {
+  flex: 0 0 50%;
+  max-width: 50%;
+}
+.filter-header {
+  margin-top: 10px;
+  margin-bottom: -10px;
+  display: block;
+}
+.faux-column.row {
+  margin-right: 0px;
+  margin-left: 0px;
+}
+.faux-column.row .custom-control {
+  margin-right: 0px;
+}
+
+</style>
