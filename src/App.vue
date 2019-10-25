b5385406125044677ba2abdc7e2ecfeb43f37b19
Kris Camilleri <kris.camilleri@gmail.com>
2019-10-25 07:36:41 +0200

    fixed XTGE search

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
         "warlock",
         "wizard"
       ],
-      sourceFilters: ["EE", "PHB", "SCAG", "XGtE"],
+      sourceFilters: ["EE", "PHB", "SCAG", "XGTE"],
       levelFilters: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       schoolFilters: [
         "Abjuration",
